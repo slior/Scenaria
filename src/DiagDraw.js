dc40e7bf3cd1dd111f52f38148585edaa202422d
slior <lior.schejter@gmail.com>
2023-11-07 23:20:44 +0200

    Rudimentary arrowhead support

--- src/DiagDraw.js
+++ src/DiagDraw.js
@@ -38,14 +38,15 @@ async function layoutModel(model)
 
     let edges = model.channels.flatMap(channel => {
         return [
-            {id : channel.from + "_" + channelID(channel), sources : [channel.from], targets : [channelID(channel)]},
-            {id : channelID(channel) + "_" + channel.to, sources : [channelID(channel)], targets : [channel.to]}
+            {id : channel.from + "_" + channelID(channel), sources : [channel.from], targets : [channelID(channel)], type : "channel"},
+            {id : channelID(channel) + "_" + channel.to, sources : [channelID(channel)], targets : [channel.to], type : "channel"}
         ]
     }).concat(model.data_flows.map(f => {
         return {
             id : f.from + "_" + f.to,
             sources : [f.from],
-            targets : [f.to]
+            targets : [f.to],
+            type : 'dataflow' //TODO: should be a constant
         }
     }))
 
@@ -85,9 +86,81 @@ function drawGraph(draw,graph)
                             [section.endPoint.x,section.endPoint.y]])
             line.stroke({width: 1, color : 'black'})
         })
+
+        if (edge.type == 'dataflow')
+        {
+            let lastSection = edge.sections[edge.sections.length-1]
+            let direction = arrowHeadDirectionFrom(lastSection)
+            drawArrowHead(draw,lastSection.endPoint.x,lastSection.endPoint.y,direction)
+        }
     })
 }
 
+const HEAD_DIRECTION = {
+    W : "W", N : "N", E : "E", S : "S"
+}
+
+function arrowHeadDirectionFrom(edgeSection)
+{
+    let x1 = edgeSection.startPoint.x
+    let y1 = edgeSection.startPoint.y
+    let x2 = edgeSection.endPoint.x
+    let y2 = edgeSection.endPoint.y
+
+    let ret = HEAD_DIRECTION.N
+    switch (true)
+    {
+        case (x2 < x1) && (y2 == y1) : ret = HEAD_DIRECTION.W; break;
+        case (x2 == x1) && (y2 < y1) : ret = HEAD_DIRECTION.N; break;
+        case (x2 == x1) && (y2 > y1) : ret = HEAD_DIRECTION.S; break;
+        case (x2 > x1) && (y2 == y1) : ret = HEAD_DIRECTION.E; break;
+    }
+
+    return ret;
+}
+
+function drawArrowHead(draw,x,y,direction)
+{
+    const ARROW_W = 10; //constants for now but should really be derived from layout - how much room it has.
+    const ARROW_H = 15;
+    //point 1 is the tip - given as parameter.
+    //point 2 is the bottom right corner, but turned according to direction
+    //point 3 is the bottom left corner, also turned.
+    let point1 = { x : x, y : y}
+    let point2 = {}
+    let point3 = {}
+
+    switch (direction)
+    {
+        case HEAD_DIRECTION.N : 
+            point2.x = x + ARROW_W/2
+            point2.y = y + ARROW_H
+            point3.x = x - ARROW_W/2
+            point3.y = y + ARROW_H
+            break;
+        case HEAD_DIRECTION.E : 
+            point2.x = x - ARROW_H
+            point2.y = y + ARROW_W/2
+            point3.x = x - ARROW_H
+            point3.y = y - ARROW_W/2
+            break;
+        case HEAD_DIRECTION.S : 
+            point2.x = x - ARROW_W/2
+            point2.y = y - ARROW_H
+            point3.x = x + ARROW_W/2
+            point3.y = y - ARROW_H
+            break;
+        case HEAD_DIRECTION.W : 
+            point2.x = x + ARROW_H
+            point2.y = y - ARROW_W/2
+            point3.x = x + ARROW_H
+            point3.y = y + ARROW_W/2
+            break;
+    }
+
+    let polylineCoords = `${point1.x},${point1.y} ${point2.x},${point2.y} ${point3.x},${point3.y}`
+    draw.polygon(polylineCoords).fill('black').stroke({width : 1})
+}
 
 function drawActor(draw,actor,actorView)
 {
